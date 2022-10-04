f0a67544bf09f8266a137db52c2af5d103882942
BenCottrell <bencottrell48@yahoo.com>
2022-10-03 23:13:42 -0400

    added while statement to determine what kind of characters will be taken from user input and created a random number generator

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -25,11 +24,55 @@ function generatePassword() {
     if (passwordLength > 7 && passwordLength < 129) {
       invalid = false;
     } else if (passwordLength == 0) {
-      Window.close;
+      Window.close();
     } else {
       alert ("Input is invalid, please input a valid character length");
     } 
   }
 }
 
-alert ("You chose the length of your password to be" + passwordLength + " characters long \n Next, confirm whether you would like your password to contain numbers, lower case, upper case, or special characters. \n You must select at least one option");
+alert ("You chose the length of your password to be \n" + passwordLength + " characters long \n Next, confirm whether you would like your password to contain numbers, lower case, upper case, or special characters. \n You must select at least one option");
+
+while (!invalid) {
+  var lowerCase = confirm("Would you like lower case characters in your password?");
+  var upperCase = confirm("Would you like upper case characters in your password?");
+  var numerical = confirm("Would you like your password to contain numbers?");
+  var special = confirm("Would you like your password to contain special characters?");
+
+  alert("You chose \n lower case letters: " + lowerCase + " \n upper case letters: " + upperCase + " \n numbers:" + numerical + "special characters: " + special);
+  if (lowerCase == true || upperCase == true || numerical == true || special == true) {
+    invalid = true;
+  } else {
+    alert ("Invalid entry. \n At least one character type must be selected.");
+  }
+}
+
+if (lowerCase == true) {
+  lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+} else {
+  lowerCase = '';
+} if (upperCase == true) {
+  upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+} else {
+  upperCase = '';
+} if (numerical == true) {
+  numerical = '123456789';
+} else {
+  numerical = '';
+} if (special == true) {
+  special = ' ~`!@#$%^&*()-+={}[]|/:;<>,.?';
+} else {
+  special = '';
+}
+
+// Merges the strings of chosen options
+var grandString = special.concat(lowerCase, upperCase, numerical);
+
+// Creates a final password by randomizing each item in the string
+{
+var randomNumber = '';
+for (var i = 0; i < passwordLength; i++) {
+  randomNumber += grandString[Math.floor(Math.random() * grandString.length)];
+} 
+return randomNumber;
+}
